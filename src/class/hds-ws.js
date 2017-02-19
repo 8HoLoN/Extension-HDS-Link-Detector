d277bbb004fbb29bc6b7630a00a0aa796d5db9de
8HoLoN <8holon@gmail.com>
2017-02-19 11:14:33 +0100

    fix getNumberOfUsers()

--- src/class/hds-ws.js
+++ src/class/hds-ws.js
@@ -37,7 +37,8 @@
     HDS.prototype.getNumberOfUsers = function(){
         return this.asyncSend('https://chrome.google.com/webstore/detail/eokdbmimjpcdehjmlcfkhcldagjpkflb')
             .then(_xhr=>{
-                var _numberOfUsers = _xhr.responseText.match(/UserDownloads:([0-9]*)/)[1];
+                //var _numberOfUsers = _xhr.responseText.match(/UserDownloads:([0-9]*)/)[1];
+                var _numberOfUsers = _xhr.responseText.match(/UserDownloads:(.*?)"/)[1];
                 //console.log(_numberOfUsers);
                 return _numberOfUsers;
             })
